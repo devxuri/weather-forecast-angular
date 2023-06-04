06efe31e562abfa9b33b398ff50437141407ff68
Suryaansh Maithani <122467397+suryaanshgm@users.noreply.github.com>
2023-06-04 20:15:11 +0100

    Components routing (#1)
    - Routing for all three pages

--- weather-forecast-app/src/app/app-routing.module.ts
+++ weather-forecast-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllCountriesComponent } from './all-countries/all-countries.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/all', pathMatch: 'full' },
+  { path: 'all', component: AllCountriesComponent },
+  { path: 'favourites', component: FavouritesComponent },
+  { path: 'weather/:countryCode', component: WeatherForecastComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
