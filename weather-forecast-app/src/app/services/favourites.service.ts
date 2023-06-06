5efdb78a3d2b652e8550f2f18a3c023a401dd494
suryaansh <suryaansh.maithani@tide.co>
2023-06-06 18:08:47 +0100

    Create Favourites service

--- weather-forecast-app/src/app/services/favourites.service.ts
+++ weather-forecast-app/src/app/services/favourites.service.ts
@@ -3,7 +3,45 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class FavouritesService {
 
-  constructor() { }
+export class FavouritesService {
+  private favouriteCountries: string[] = [];
+
+  constructor() {
+    this.loadFavourites();
+  }
+
+  addToFavourites(countryCode: string): void {
+    if (!this.isFavourite(countryCode)) {
+      this.favouriteCountries.push(countryCode);
+      this.saveFavourites();
+    }
+  }
+
+  removeFromFavourites(countryCode: string): void {
+    const index = this.favouriteCountries.indexOf(countryCode);
+    if (index !== -1) {
+      this.favouriteCountries.splice(index, 1);
+      this.saveFavourites();
+    }
+  }
+
+  getFavouriteCountries(): string[] {
+    return this.favouriteCountries;
+  }
+
+  isFavourite(countryCode: string): boolean {
+    return this.favouriteCountries.includes(countryCode);
+  }
+
+  private loadFavourites(): void {
+    const favourites = localStorage.getItem('favourites');
+    if (favourites) {
+      this.favouriteCountries = JSON.parse(favourites);
+    }
+  }
+
+  private saveFavourites(): void {
+    localStorage.setItem('favourites', JSON.stringify(this.favouriteCountries));
+  }
 }
