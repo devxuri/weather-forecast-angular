3d90df8ebfee3f1e9595f1ca832309153667c3e4
Suryaansh Maithani <122467397+suryaanshgm@users.noreply.github.com>
2023-06-07 20:40:40 +0100

    Prod feature favourites (#5)
    * Basic favourites display functionality & Services restructure
    
    - Weather services broken into country and weather service
    - Basic favourites page created with minimal functionality
    
    * Added minor website icon
    
    * Working favourites feature
    
    - Added an input option to country-card to change behaviour based on page
    - Favourites feature fully working

--- weather-forecast-app/src/app/components/common/all-countries/all-countries.component.ts
+++ weather-forecast-app/src/app/components/common/all-countries/all-countries.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RoutingService } from '../../../services/routing.service';
-import { WeatherService } from '../../../services/weather.service';
+import { CountriesService } from '../../../services/countries.service';
 
 import { Observable, of } from 'rxjs';
 
@@ -18,7 +18,7 @@ export class AllCountriesComponent {
 
   constructor(
     private routingService: RoutingService,
-    private weatherService: WeatherService,
+    private countriesService: CountriesService,
   ) {}
 
   ngOnInit(): void {
@@ -30,7 +30,7 @@ export class AllCountriesComponent {
   }
 
   subscribeToAllCountries(): void {
-    this.weatherService.getAllCountries().subscribe({
+    this.countriesService.getAllCountries().subscribe({
       next: (data: any[]) => {
         this.countriesData = data;
       },
@@ -44,7 +44,7 @@ export class AllCountriesComponent {
     clearTimeout(this.searchTimeout);
     this.searchTimeout = setTimeout(() => {
       if (this.searchText) {
-        this.filteredCountries = this.weatherService.getCountriesByFilter(this.searchText);
+        this.filteredCountries = this.countriesService.getCountriesByFilterName(this.searchText);
       }
     }, 200);
   }
--- weather-forecast-app/src/app/services/weather.service.ts
+++ weather-forecast-app/src/app/services/weather.service.ts
@@ -1,53 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { formatDate } from '@angular/common';
-import { Observable, of } from 'rxjs';
+import { Observable, forkJoin, of } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import { ConfigService } from '../config/config.service';
+import { CountriesService } from './countries.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WeatherService {
+export class WeatherService  {
   
-    constructor(private http: HttpClient, private configService: ConfigService) { }
+    constructor(private http: HttpClient, private configService: ConfigService, private countriesService: CountriesService) { }
   
-    getAllCountries(): Observable<any[]> {
-      return this.http.get<any[]>(this.configService.allCountriesUrl).pipe(
-        map((response: any[]) => {
-          return response.map(country => {
-            const flag = country.flags?.svg || '';
-            const name = country.name?.common || '';
-            const capital = country.capital?.[0] || '';
-            const code = country.altSpellings?.[0] || '';
-            const lat = country.latlng?.[0] || '';
-            const lng = country.latlng?.[1] || '';
-
-            return {
-              flag,
-              name,
-              capital,
-              code,
-              lat,
-              lng
-            };
-          }).sort((a, b) => a.name.localeCompare(b.name));
-        }),
-        catchError(error => {
-          console.error('Error fetching data:', error);
-          return of([]);
-        })
-      );
-    }
-
-    getCountriesByFilter(filterText: string): Observable<any[]> {
-      return this.getAllCountries().pipe(
-        map(countries => countries.filter(country => country.name.toLowerCase().includes(filterText.toLowerCase())))
-      );
-    }
-
     getWeatherForecast(code: string): Observable<any> {
-      return this.getAllCountries().pipe(
+      return this.countriesService.getAllCountries().pipe(
         map(countries => countries.find(country => country.code === code)),
         switchMap(country => {
           const weatherApiRequest = `${this.configService.weatherUrl}lat=${country.lat}&lon=${country.lng}`;
